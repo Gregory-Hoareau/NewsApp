b8f9a31bb4133f3aa0cafa3a772c799a2dbbe15b
Gregory-Hoareau <66921801+Gregory-Hoareau@users.noreply.github.com>
2022-09-10 15:52:01 +0200

    Creating news model

---
+++ NewsApp/Models/News.swift
@@ -0,0 +1,13 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by MacBook on 10/09/2022.
+//
+
+struct News {
+    var title: String
+    var description: String
+    var url: String
+    var urlToImage: String
+}
