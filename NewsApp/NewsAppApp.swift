d0641fd12c8db82043126f83be6aa50d11aa3297
Gregory-Hoareau <66921801+Gregory-Hoareau@users.noreply.github.com>
2022-09-10 15:11:52 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by MacBook on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
